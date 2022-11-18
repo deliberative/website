da0dbed7fca365f54181a0abb8b8538f2e31bd28
fuzzc0re <dim@karoukis.com>
2022-11-18 16:27:04 +0200

    Removed subscription

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,9 +1,8 @@
 <script>
-  import Subscription from './Subscription.svelte';
+  /* import Subscription from './Subscription.svelte'; */
   import Copyright from './Copyright.svelte';
 </script>
 
 <footer class="bg-white dark:bg-gray-900 body-font fade-in">
-  <Subscription />
   <Copyright />
 </footer>
